db1c0fe34a5478999fc4325aaf2db2ce426e6cc1
Adam Xiao <adam.x.xiao@gmail.com>
2020-02-18 17:55:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-/* Your code goes here */
+var customerName = "bob";
+const leastFavoriteCustomer = "adam";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(customer){
+  bestCustomer = customer;
+}
+
+function changeLeastFavoriteCustomer(customer){
+  leastFavoriteCustomer = customer;
+}
